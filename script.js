2dd939e5392d84502db50dd29360bdeab0e2fe11
BlackCNP <90748408+BlackCNP@users.noreply.github.com>
2024-09-23 16:15:36 +0300

    setup eslint

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 "use strict";
-// типи
+//  типи
 const modal = document.getElementById('myModal');
 const openButton = document.getElementById('openModal');
 const closeButton = document.getElementById('closeModal');
-//модальне
+// Відкриття модального вікна
 openButton === null || openButton === void 0 ? void 0 : openButton.addEventListener('click', () => {
     if (modal) {
         modal.style.display = 'block';
     }
 });
-
 closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener('click', () => {
     if (modal) {
         modal.style.display = 'none';
     }
 });
-// Витягування даних  fetch 
+// Витягування даних  fetch
 fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(data => {
-    console.log(data); // Виведення даних з API в консоль
+    console.log(data);
 })
     .catch(error => {
     console.error('Error fetching data:', error);
