8f7acff0f7faca68b44ba030fc72535f40a44644
BlackCNP <90748408+BlackCNP@users.noreply.github.com>
2024-09-23 14:30:54 +0300

    add interactivity

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+// типи
+const modal = document.getElementById('myModal');
+const openButton = document.getElementById('openModal');
+const closeButton = document.getElementById('closeModal');
+//модальне
+openButton === null || openButton === void 0 ? void 0 : openButton.addEventListener('click', () => {
+    if (modal) {
+        modal.style.display = 'block';
+    }
+});
+
+closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener('click', () => {
+    if (modal) {
+        modal.style.display = 'none';
+    }
+});
+// Витягування даних  fetch 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => {
+    console.log(data); // Виведення даних з API в консоль
+})
+    .catch(error => {
+    console.error('Error fetching data:', error);
+});
